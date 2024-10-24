5031bd243ce2abd457299fde67e5ae64c3aaa8f8
apriladzani <april.adzania@gmail.com>
2024-10-24 13:19:00 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,21 @@
 public class Nomor1{
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
 
+        int jumlahAnggotaKeluarga;
+        double tunjanganKesehatan;
+
+        System.out.print("Jumlah anggota keluarga:");
+        jumlahAnggotaKeluarga = scanner.nextInt();
+
+        if (jumlahAnggotaKeluarga <= 3) {
+            tunjanganKesehatan = 100000 * jumlahAnggotaKeluarga;
+        }else {
+            tunjanganKesehatan = (100000 * jumlahAnggotaKeluarga) - ((jumlahAnggotaKeluarga - 3) * 50000);
+        }
+
+        System.out.println("Besar tunjangan kesehatan = " + tunjanganKesehatan);
+
+        scanner.close();
     }
 }
